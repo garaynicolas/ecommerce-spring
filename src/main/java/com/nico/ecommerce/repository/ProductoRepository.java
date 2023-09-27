06992a694438b11b755b589bfb15229ab48f5769
Nico Garay <garay.nicolas002@gmail.com>
2023-09-27 12:16:07 -0300

    repository

---
+++ src/main/java/com/nico/ecommerce/repository/ProductoRepository.java
@@ -0,0 +1,10 @@
+package com.nico.ecommerce.repository;
+
+import com.nico.ecommerce.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+
+}
