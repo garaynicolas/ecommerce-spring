6027e86618eeee753a9a871771d5027f4aa24a84
Nico Garay <garay.nicolas002@gmail.com>
2023-09-27 11:14:29 -0300

    templates

--- src/main/java/com/nico/ecommerce/model/Producto.java
+++ src/main/java/com/nico/ecommerce/model/Producto.java
@@ -1,6 +1,12 @@
 package com.nico.ecommerce.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="productos")
 public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
     private String descripcion;
@@ -8,16 +14,20 @@ public class Producto {
     private double precio;
     private int cantidad;
 
+    @ManyToOne
+    private Usuario usuario;
+
     public Producto() {
     }
 
-    public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad) {
+    public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad, Usuario usuario) {
         this.id = id;
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.imagen = imagen;
         this.precio = precio;
         this.cantidad = cantidad;
+        this.usuario = usuario;
     }
 
     public Integer getId() {
@@ -67,6 +77,16 @@ public class Producto {
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
     }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+
 
     @Override
     public String toString() {
